bf45fb21b2cf5bdf6f812f4e617010ab584d3cdb
Bryce-KF <brycefiser@gmail.com>
2024-12-02 20:33:36 +0000

    Final finished

--- dragon/assets/js/scripts.js
+++ dragon/assets/js/scripts.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function() {
-
+/*
     $('.fade').slick({
       dots: true,
       infinite: true,
@@ -11,6 +11,35 @@ $(document).ready(function() {
       autoplay: true,
       autoplaySpeed: 2000
     });
-
+*/
 
 });
+$('.fade').slick({
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        dots: true,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+  
+});
+
